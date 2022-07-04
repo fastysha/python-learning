ba39b5f5c0fd3d1fc7b6adfb66d7a7c1572bb2c1
fastysha <skutelnikov.and7@gmail.com>
2022-07-04 18:17:34 +0300

    math game v4

--- games/math-game/game.py
+++ games/math-game/game.py
@@ -1,6 +1,7 @@
 import random
+from datetime import datetime
 
-QUESTIONS_COUNT = 20
+QUESTIONS_COUNT = 5
 MIN_N = 1
 MAX_N = 10
 
@@ -14,15 +15,15 @@ sign_dict = {
     "*": lambda a, b: a*b
 }
 
-grade_dict={
-    range(90,101):"A",
-    range(80,90):"B",
-    range(75,80):"C",
-    range(65,75):"D",
-    range(60,65):"E",
-    range(0,60):"F"
+grade_dict = {
+    range(90, 101): "A",
+    range(80, 90): "B",
+    range(75, 80): "C",
+    range(65, 75): "D",
+    range(60, 65): "E",
+    range(0, 60): "F"
 }
-
+start = datetime.now()
 while question_number <= QUESTIONS_COUNT:
     left_num = random.randint(MIN_N, MAX_N)
     right_num = random.randint(MIN_N, MAX_N)
@@ -30,13 +31,13 @@ while question_number <= QUESTIONS_COUNT:
 
     print(question_number, ").", left_num, sign, right_num, "=")
     answer = int(input(""))
-    
+
     if sign_dict.get(sign)(left_num, right_num) == answer:
         correct_answers_counter += 1
-    
+
     question_number += 1
 
-percent_num=(correct_answers_counter*100)/QUESTIONS_COUNT
+percent_num = (correct_answers_counter*100)/QUESTIONS_COUNT
 
 grade = ''
 for range_key in grade_dict.keys():
@@ -44,4 +45,6 @@ for range_key in grade_dict.keys():
         grade = grade_dict[range_key]
         break
 
-print(correct_answers_counter, "correct out of", QUESTIONS_COUNT, ". Grade:", grade)
+print(correct_answers_counter, "correct out of",
+      QUESTIONS_COUNT, ". Grade:", grade)
+print("Time used: ", (datetime.now() - start).seconds, "sec")
