f3f0355278ec6b32f1fe3c3e0888d71b1d13e928
fastysha <skutelnikov.and7@gmail.com>
2022-08-10 17:36:33 +0300

    Added logic to send a Parcel

--- smart_post/sm_user.py
+++ smart_post/sm_user.py
@@ -1,30 +1,56 @@
+from ast import While
 from secrets import choice
-from sm_dbconnector import find_user_by_phone_number
+from sm_dbconnector import create_parcel, find_user_by_phone_number
 from sm_dbconnector import create_user
+from sm_dbconnector import create_parcel
+from sm_data_models import ParcelStatus
+
+####################################################################
 print("Привет это Smart Post")
 
-user_phone_number=input("Введите свой номер: ")
+user_phone_number = input("Введите свой номер: ")
 
-user = find_user_by_phone_number(user_phone_number)
+current_user = find_user_by_phone_number(user_phone_number)
 
-if user is None:
+if current_user is None:
     print('Такого пользователя не существует.Зарегистрируйтесь!')
-    name=input("Введите имя: ")
-    surname=input("Введите фамилию: ")
-    phone_number=input("Номер телефона:")
-    country=input("Ваша страна: ")
+    name = input("Введите имя: ")
+    surname = input("Введите фамилию: ")
+    phone_number = input("Номер телефона:")
+    country = input("Ваша страна: ")
 
-    new_user={
+    new_user = {
         "name": name,
-        "surname":surname,
-        "phone_num":phone_number,
-        "country":country
-        }
+        "surname": surname,
+        "phone_num": phone_number,
+        "country": country
+    }
 
     create_user(new_user)
 else:
-    print('Привет,', user["UserName"], user["UserSurname"])
-    print("Выберите действие: 1-Мои посылки,2-Отправить посылку")
-    choice=int(input())
-
-
+    while True:
+        print('Привет,', current_user["UserName"], current_user["UserSurname"])
+        print("Выберите действие: 1-Мои посылки,2-Отправить посылку")
+        choice = int(input())
+        if choice == 1:
+            
+        elif choice == 2:
+            phone_num_des = input("Номер телефона получателя: ")
+            consumer = find_user_by_phone_number(phone_num_des)
+            if consumer is None:
+                print("Пользователь не найден в системе")
+            else:
+                print(f"Посылка поедет к: {consumer['UserName']} {consumer['UserSurname']}")
+                name_parcel = input("Название посылки: ")
+                price = input("Оценочная стоимость: ")
+                post_num = input("Номер отделения: ")
+                new_parcel = {
+                    "name": name_parcel,
+                    "phone": phone_num_des,
+                    "price": price,
+                    "post_num": post_num,
+                    "sender_id":current_user["UserID"],
+                    "consumer_id":consumer["UserID"],
+                    "status":ParcelStatus.IN_PROGRESS.name
+                }
+                create_parcel(new_parcel)
