3db76fcf53865e957f749af0edd5e55c7a6e5b90
fastysha <skutelnikov.and7@gmail.com>
2022-08-10 17:36:49 +0300

    Improves

--- smart_post/sm_dbconnector.py
+++ smart_post/sm_dbconnector.py
@@ -2,26 +2,30 @@ from sm_data_models import ParcelStatus
 import mysql.connector
 
 db = mysql.connector.connect(
-  host="localhost",
-  user="admin",
-  password="admin",
-  database="mydatabase"
+    host="localhost",
+    user="admin",
+    password="admin",
+    database="mydatabase"
 )
 
 ############################################
 ############# Users Table SQL ##############
-# ########################################## 
+# ##########################################
+
+
 def find_user_by_phone_number(phone_number):
     cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM Users WHERE UserPhoneNumber = %s", (phone_number,))
+    cursor.execute(
+        "SELECT * FROM Users WHERE UserPhoneNumber = %s", (phone_number,))
     user = cursor.fetchone()
     # print(f'Finded user by phone {phone_number} is: {user}')
     return user
 
-def create_user(new_user):
+
+def create_user(user):
     cursor = db.cursor()
-    sql = "INSERT INTO Users (UserName, UserSurname,UserPhoneNumber,Country) VALUES (%s, %s,%s,%s)"
-    args = (new_user["name"],new_user["surname"],new_user["phone_num"],new_user["country"])
+    sql = "INSERT INTO Users (UserName, UserSurname,UserPhoneNumber,Country) VALUES (%s,%s,%s,%s)"
+    args = (user["name"], user["surname"], user["phone_num"], user["country"])
     cursor.execute(sql, args)
     db.commit()
     print(cursor.rowcount, "record inserted.")
@@ -29,18 +33,29 @@ def create_user(new_user):
 
 ############################################
 ############# Parcels Table SQL ############
-# ##########################################     
-def find_all_not_delivered_parcels() -> list:
-    cursor = db.cursor(dictianory=True)
-    sql = "SELECT * FROM Parcels WHERE Status = %s AND todo records those live for more than 3 minutes "
-    cursor.execute(sql, (ParcelStatus.IN_PROGRESS.name, ))
+# ##########################################
+def create_parcel(new_parcel):
+    cursor = db.cursor()
+    sql = "INSERT INTO parcels(SenderID,ConsumerID,ParcelName,Price,DestinationPostNumber,Status) VALUES (%s,%s,%s,%s,%s,%s)"
+    args = (new_parcel["sender_id"], new_parcel["consumer_id"], new_parcel["name"],
+            new_parcel["price"], new_parcel["post_num"], new_parcel["status"])
+    cursor.execute(sql, args)
+    db.commit()
+    print(cursor.rowcount, "record inserted.")
+
+
+def find_all_not_delivered_parcels(delivery_time_seconds: int) -> list:
+    cursor = db.cursor(dictionary=True)
+    sql = "SELECT * FROM Parcels WHERE Status = %s AND CreationDate <= (CURRENT_TIMESTAMP() - INTERVAL %s SECOND)"
+    cursor.execute(
+        sql, (ParcelStatus.IN_PROGRESS.name, delivery_time_seconds,))
     parcels = cursor.fetchall()
     return parcels
 
 
-def update_parcels(status: str, delivery_time_seconds: int, parcel_ids: list):
+def update_parcels(status: str, parcel_ids: list):
     cursor = db.cursor()
-    sql = "UPDATE Parcels SET Status = %s, DeliveryDate >= (CURRENT_TIMESTAMP() - INTERVAL %s SECONDS) WHERE ParcelId IN %s"
-    args = (status, delivery_time_seconds, parcel_ids,)
+    sql = "UPDATE Parcels SET Status = %s, DeliveryDate = CURRENT_TIMESTAMP() WHERE ParcelId IN (%s)"
+    args = (status, ",".join(parcel_ids),)
     cursor.execute(sql, args)
-    db.commit()    
+    db.commit()
--- smart_post/sm_delivery_service.py
+++ smart_post/sm_delivery_service.py
@@ -2,16 +2,15 @@ import time
 from sm_dbconnector import find_all_not_delivered_parcels, update_parcels
 from sm_data_models import ParcelStatus
 
-
 DELIVERY_TIME_SECONDS = 90
 
 # Этот сервис имитирует работу грузовой машины которая идете и доставляет посылки
 # Доставленной считается посылка которая едет уже энное время, см. DELIVERY_TIME_SECONDS
 while True:
     print('Ваши посылки в пути...')
-    delivered_parcels = find_all_not_delivered_parcels()
+    delivered_parcels = find_all_not_delivered_parcels(DELIVERY_TIME_SECONDS)
     if delivered_parcels:
-        parcel_ids = [str(p['ParcelId']) for p in delivered_parcels]
+        parcel_ids = [str(p['ParcelID']) for p in delivered_parcels]
         print('Посылки приехали:', parcel_ids)
-        update_parcels(delivered_parcels, ParcelStatus.DELIVERED.name)
+        update_parcels(ParcelStatus.DELIVERED.name, parcel_ids)
     time.sleep(10)
